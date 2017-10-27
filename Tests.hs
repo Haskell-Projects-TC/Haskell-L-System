00c259865dc349782b81688cded89a8126895a63
Tiger Cross <tc2017@edge05.doc.ic.ac.uk>
2017-10-27 18:40:14 +0100

    implemented, added tests, extension done

--- Tests.hs
+++ Tests.hs
@@ -11,6 +11,7 @@ angleTestCases
     , (1, "", []) ==> 1
     , triangle    ==> 90
     , arrowHead   ==> 60
+    , bush        ==> 22.5
     ]
 
 baseTestCases
@@ -18,6 +19,8 @@ baseTestCases
     , cross        ==> "M-M-M-M"
     , triangle     ==> "-M"
     , arrowHead    ==> "N"
+    , bush         ==> "X"
+    , (0, "", [])  ==> ""
     ]
 
 rulesTestCases
@@ -27,6 +30,15 @@ rulesTestCases
                 ]
     , (0, "", [ ('M', "N") ])
         ==> [ ('M', "N") ]
+    , bush ==> [ ('X', "M-[[X]+X]+M[+MX]-X")
+               , ('M', "MM")
+               , ('[', "[")
+               , (']', "]")
+               , ('+', "+")
+               , ('-', "-")
+               ]
+    , (0, "", []) 
+         ==> []
     ]
 
 {- Note: these test cases use angle/base/rules, and will fail the test
@@ -42,12 +54,15 @@ lookupCharTestCases
       ==> "M+N++N-M--MM-N+"
     , ('+', (rules triangle))
       ==> "+"
+    , ('X',  (rules bush))
+      ==> "M-[[X]+X]+M[+MX]-X"
     ]
 
 expandOneTestCases
   = [ (rules triangle, base triangle)
-        ==> "-M+M-M-M+M"
+        ==> "-M+M-M-M+M"             
     , ([('A', "B")], "A") ==> "B"
+    , ([], "")            ==> ""
     ]
 
 expandTestCases
@@ -66,12 +81,18 @@ expandTestCases
                    , "-A-A+A+A+MX--MY+--A-MX++MY-+--A-A+MX--MY+++A-MX++MY--++"
                    , "+A-A+A+MX--MY+--A-MX++MY-+++A-A+MX--MY+++A-MX++MY----+"
                    ]
+    , ([], "", 50) 
+        ==> ""
+    , (rules bush, base bush, 4)
+        ==> "MMMMMMMM-[[MMMM-[[MM-[[M-[[X]+X]+M[+MX]-X]+M-[[X]+X]+M[+MX]-X]+MM[+MMM-[[X]+X]+M[+MX]-X]-M-[[X]+X]+M[+MX]-X]+MM-[[M-[[X]+X]+M[+MX]-X]+M-[[X]+X]+M[+MX]-X]+MM[+MMM-[[X]+X]+M[+MX]-X]-M-[[X]+X]+M[+MX]-X]+MMMM[+MMMMMM-[[M-[[X]+X]+M[+MX]-X]+M-[[X]+X]+M[+MX]-X]+MM[+MMM-[[X]+X]+M[+MX]-X]-M-[[X]+X]+M[+MX]-X]-MM-[[M-[[X]+X]+M[+MX]-X]+M-[[X]+X]+M[+MX]-X]+MM[+MMM-[[X]+X]+M[+MX]-X]-M-[[X]+X]+M[+MX]-X]+MMMM-[[MM-[[M-[[X]+X]+M[+MX]-X]+M-[[X]+X]+M[+MX]-X]+MM[+MMM-[[X]+X]+M[+MX]-X]-M-[[X]+X]+M[+MX]-X]+MM-[[M-[[X]+X]+M[+MX]-X]+M-[[X]+X]+M[+MX]-X]+MM[+MMM-[[X]+X]+M[+MX]-X]-M-[[X]+X]+M[+MX]-X]+MMMM[+MMMMMM-[[M-[[X]+X]+M[+MX]-X]+M-[[X]+X]+M[+MX]-X]+MM[+MMM-[[X]+X]+M[+MX]-X]-M-[[X]+X]+M[+MX]-X]-MM-[[M-[[X]+X]+M[+MX]-X]+M-[[X]+X]+M[+MX]-X]+MM[+MMM-[[X]+X]+M[+MX]-X]-M-[[X]+X]+M[+MX]-X]+MMMMMMMM[+MMMMMMMMMMMM-[[MM-[[M-[[X]+X]+M[+MX]-X]+M-[[X]+X]+M[+MX]-X]+MM[+MMM-[[X]+X]+M[+MX]-X]-M-[[X]+X]+M[+MX]-X]+MM-[[M-[[X]+X]+M[+MX]-X]+M-[[X]+X]+M[+MX]-X]+MM[+MMM-[[X]+X]+M[+MX]-X]-M-[[X]+X]+M[+MX]-X]+MMMM[+MMMMMM-[[M-[[X]+X]+M[+MX]-X]+M-[[X]+X]+M[+MX]-X]+MM[+MMM-[[X]+X]+M[+MX]-X]-M-[[X]+X]+M[+MX]-X]-MM-[[M-[[X]+X]+M[+MX]-X]+M-[[X]+X]+M[+MX]-X]+MM[+MMM-[[X]+X]+M[+MX]-X]-M-[[X]+X]+M[+MX]-X]-MMMM-[[MM-[[M-[[X]+X]+M[+MX]-X]+M-[[X]+X]+M[+MX]-X]+MM[+MMM-[[X]+X]+M[+MX]-X]-M-[[X]+X]+M[+MX]-X]+MM-[[M-[[X]+X]+M[+MX]-X]+M-[[X]+X]+M[+MX]-X]+MM[+MMM-[[X]+X]+M[+MX]-X]-M-[[X]+X]+M[+MX]-X]+MMMM[+MMMMMM-[[M-[[X]+X]+M[+MX]-X]+M-[[X]+X]+M[+MX]-X]+MM[+MMM-[[X]+X]+M[+MX]-X]-M-[[X]+X]+M[+MX]-X]-MM-[[M-[[X]+X]+M[+MX]-X]+M-[[X]+X]+M[+MX]-X]+MM[+MMM-[[X]+X]+M[+MX]-X]-M-[[X]+X]+M[+MX]-X"
     ]
 
 moveTestCases
-  = [ ('L', ((100, 100), 90), 90) ==> ((100.0,100.0),180.0)
-    , ('F', ((50,50), 60), 60)    ==> ((50.5,50.866024),60.0)
-    , ('F', ((-25,180),180), 45)  ==> ((-26.0,180.0),180.0)
+  = [ ('L', ((100, 100), 90), 90)  ==> ((100.0,100.0),180.0)
+    , ('F', ((50,50), 60), 60)     ==> ((50.5,50.866024),60.0)
+    , ('F', ((-25,180),180), 45)   ==> ((-26.0,180.0),180.0)
+    , ('R', ((12, 12), 90), 90)    ==> ((12.0,12.0),0.0)
+    , ('F',((-74,-62), 48), (-23)) ==> ((-73.33087,-61.256855),48.0)
     ]
 
 traceTestCases
@@ -89,9 +110,17 @@ traceTestCases
                 ,((-4.371139e-8,1.0),(0.7071067,1.7071068),(1.0,0.0,0.0))
                 ,((-4.371139e-8,1.0),(-0.7071068,1.7071068),(1.0,0.0,0.0))
                 ,((-4.371139e-8,1.0),(-8.742278e-8,2.0),(1.0,0.0,0.0))
-                ,((-8.742278e-8,2.0),(-1.3113416e-7,3.0),(1.0,0.0,0.0))]
+                ,((-8.742278e-8,2.0),(-1.3113416e-7,3.0),(1.0,0.0,0.0))
+                ],
+       ((expandOne mapper (expand (rules bush)
+            (base bush) 1)), (angle bush), blue)
+      ==> sort [ ((-8.742278e-8,2.0),(-0.3826835,2.9238796),(0.0,0.0,1.0))
+                ,((-4.371139e-8,1.0),(-8.742278e-8,2.0),(0.0,0.0,1.0))
+                ,((0.0,0.0),(-4.371139e-8,1.0),(0.0,0.0,1.0))
+               ],
+      ( "", 4, blue)
+      ==> []
     ]
-
 
 allTestCases
   = [ TestCase "angle"      (angle . unId)
